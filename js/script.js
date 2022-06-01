1ac3cd95b6f8b11031dfe40b788810bad3ba72f4
Ryskulov Talant <rtalant02@gmail.com>
2022-06-02 05:43:06 +0600

    Страница О нас готова, торты в разработке

--- js/script.js
+++ js/script.js
@@ -4,47 +4,47 @@
 const sliderImage = document.querySelector('.slider__image');
 const slides = sliderImage.getElementsByTagName('img');
 var i = 0;
-
-function prevSlide() {
-  slides[i].classList.remove('active');
-  i = (i - 1 + slides.length) % slides.length;
-  slides[i].classList.add('active');
-}
 function nextSlide() {
   slides[i].classList.remove('active');
   i = (i + 1) % slides.length;
   slides[i].classList.add('active');
 }
+function prevSlide() {
+  slides[i].classList.remove('active');
+  i = (i - 1 + slides.length) % slides.length;
+  slides[i].classList.add('active');
+}
+
 //-ТЕКСТ
 const sliderBody = document.querySelector('.slider__body');
 const slidesText = sliderBody.getElementsByClassName('slider__text');
 var j = 0;
+function nextSlideText() {
+  slidesText[j].classList.remove('active');
+  j = (j + 1) % slidesText.length;
+  slidesText[j].classList.add('active');
+}
 
 function prevSlideText() {
   slidesText[j].classList.remove('active');
   j = (j - 1 + slidesText.length) % slidesText.length;
   slidesText[j].classList.add('active');
 }
-function nextSlideText() {
-  slidesText[j].classList.remove('active');
-  j = (j + 1) % slidesText.length;
-  slidesText[j].classList.add('active');
-}
 //-ТОЧКИ
-const sliderControls = document.querySelector('.slider__controls');
-const dots = sliderControls.querySelectorAll('slider__dots');
+const sliderControls = document.querySelector('.slider__all-dots');
+const dots = sliderControls.querySelectorAll('.slider__dots');
 var sliderNav = function (manual) {
   dots.forEach((dot) =>{
     dot.classList.remove('active');
   });
-  slides.forEach((slide)=>{
-    slide.classList.remove('active');
+  dots[manual].classList.add('active');
+  sliderImage.forEach((slides)=>{
+    slides.classList.remove('active');
   });
+  sliderImage[manual].classList.add('active');
   slidesText.forEach((text)=>{
     text.classList.remove('active');
   })
-  dots[manual].classList.add('active');
-  slides[manual].classList.add('active');
   slidesText[manual].classList.add('active');
 }
 dots.forEach((dot, k) => {
